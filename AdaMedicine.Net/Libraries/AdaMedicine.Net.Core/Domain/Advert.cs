06ac457e1595c000d1bd8f2229e0725647b282f0
ihsan çungay <ihsan_cungay@hotmail.com>
2021-02-28 18:59:55 +0300

    Init AdaMedicine.Net.Core

---
+++ AdaMedicine.Net/Libraries/AdaMedicine.Net.Core/Domain/Advert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AdaMedicine.Net.Core.Domain
+{
+    public partial class Advert : BaseEntity
+    {
+        public int HospitalId { get; set; }
+
+        public string Title { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedOnUtc { get; set; }
+    }
+}

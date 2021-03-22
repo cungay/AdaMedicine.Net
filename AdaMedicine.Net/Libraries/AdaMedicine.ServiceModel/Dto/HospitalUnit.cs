93e3a3a7211c0a0d8c0156a1bc6a19843bc023cc
ihsan çungay <ihsan_cungay@hotmail.com>
2021-03-22 23:27:19 +0300

    Create HospitalUnit.cs

---
+++ AdaMedicine.Net/Libraries/AdaMedicine.ServiceModel/Dto/HospitalUnit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace AdaMedicine.ServiceModel.Dto
+{
+    [DataContract]
+    public class HospitalUnit
+    {
+        public HospitalUnit()
+        {
+            this.SubCategories = new List<UnitCategoryDto>();
+            this.Category = new UnitCategoryDto();
+        }
+
+        public UnitCategoryDto Category { get; set; }
+
+        public List<UnitCategoryDto> SubCategories { get; set; }
+    }
+}

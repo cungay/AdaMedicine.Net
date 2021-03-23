006cc879c32b9a8836b2f1f7d69e4015831895ba
ihsan çungay <ihsan_cungay@hotmail.com>
2021-03-23 14:03:26 +0300

    Create GetUnits.cs

---
+++ AdaMedicine.Net/Libraries/AdaMedicine.ServiceModel/Request/GetUnits.cs
@@ -0,0 +1,13 @@
+﻿using Ege.Net;
+using AdaMedicine.ServiceModel.Dto;
+
+namespace AdaMedicine.ServiceModel.Request
+{
+    [Route("/hospital-units", "GET", Summary = "Get all hospital units")]
+    [Route("/hospital-units/{id}", "GET", Summary = "Get all hospital units by id")]
+    public class GetUnits : IReturn<ListResponse<HospitalUnitDto>>
+    {
+        [ApiMember(ParameterType = "path", DataType = "integer", Format = "int32", IsRequired = false, Route = "/hospital-units/{id}")]
+        public int? Id { get; set; }
+    }
+}

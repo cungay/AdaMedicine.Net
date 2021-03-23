02b552b497f7fcde1c56942a0cec9ca8a5155aba
ihsan çungay <ihsan_cungay@hotmail.com>
2021-03-23 14:04:09 +0300

    Update HospitalUnitService.cs

--- AdaMedicine.Net/Libraries/AdaMedicine.Services/HospitalUnitService.cs
+++ AdaMedicine.Net/Libraries/AdaMedicine.Services/HospitalUnitService.cs
@@ -5,27 +5,33 @@ using AdaMedicine.ServiceModel.Dto;
 using AdaMedicine.ServiceModel.Request;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace AdaMedicine.Services
 {
     public class HospitalUnitService : RestService
     {
-        public async Task<SingleResponse<HospitalUnitDto>> Get(GetHospitalUnit request)
+        public async Task<ListResponse<HospitalUnitDto>> Get(GetUnits request)
         {
-            var response = new SingleResponse<HospitalUnitDto>();
-            var unitCategory = await Task.FromResult(Db.SingleAsync<UnitCategory>
-                (p => p.Id == request.Id && p.Published && !p.Deleted)).Result;
-            if (unitCategory == null)
-                return new SingleResponse<HospitalUnitDto>();
-            var subCategories = Db.SelectAsync(Db.From<UnitCategory>().OrderBy(p => p.CategoryName)
-                .Where(p => Sql.In(p.CategoryId, unitCategory.Id)));
-            var hospitalUnitLookup = await Task.FromResult(subCategories.Result.ToLookup(o => o.CategoryId));
-            HospitalUnitDto dto = new()
-            {
-                Category = unitCategory.ConvertTo<UnitCategoryDto>(),
-                SubCategories = hospitalUnitLookup[unitCategory.Id].ToList().ConvertAll(p => p.ConvertTo<UnitCategoryDto>())
-            };
-            response.Result = dto;
+            ListResponse<HospitalUnitDto> response = new() { Result = new List<HospitalUnitDto>() };
+            var query = Db.From<UnitCategory>();
+            if (!request.Id.IsNull())
+                query.Where(p => p.Id == request.Id);
+            query.Where(Sql.Custom("SubCategoryId IS NULL"));
+            query.Where(p => p.Published && !p.Deleted);
+            var units = await Task.FromResult(Db.SelectAsync(query).Result.ConvertAll(p => p.ConvertTo<UnitCategoryDto>()));
+            if (units.Count > 0) {
+                var details = await Task.FromResult(Db.SelectAsync(Db.From<UnitCategory>()
+                    .OrderBy(p => p.CategoryName)
+                    .Where(p => Sql.In(p.CategoryId, units.ConvertAll(p => p.Id)))
+                    .Where(p => p.Published && !p.Deleted)).Result.ConvertAll(p => p.ConvertTo<UnitCategoryDto>()));
+                var hospitalUnitLookup = details.ToLookup(o => o.CategoryId);
+                var dto = units.ConvertAll(o => new HospitalUnitDto {
+                    Unit = o,
+                    Details = hospitalUnitLookup[o.Id].ToList()
+                });
+                response.Result = dto;
+            }
             return response;
         }
     }

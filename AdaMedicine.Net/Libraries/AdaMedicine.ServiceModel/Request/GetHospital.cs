56f64cfc17c6663a8e257cb7e82f9765fea1a9a6
ihsan çungay <ihsan_cungay@hotmail.com>
2021-03-23 14:03:12 +0300

    Update GetHospital.cs

--- AdaMedicine.Net/Libraries/AdaMedicine.ServiceModel/Request/GetHospital.cs
+++ AdaMedicine.Net/Libraries/AdaMedicine.ServiceModel/Request/GetHospital.cs
@@ -3,11 +3,10 @@ using AdaMedicine.ServiceModel.Dto;
 
 namespace AdaMedicine.ServiceModel.Request
 {
-    //[Tag("Hospital Requests")]
-    [Route("/hospitals/{id}", "GET", Summary = "Get hospital by id")]
+    [Route("/hospitals/{hospitalid}", "GET", Summary = "Get hospital by id")]
     public class GetHospital : IReturn<SingleResponse<HospitalDto>>
     {
-        [ApiMember(ParameterType = "path", DataType = "integer", Format = "int32", IsRequired = false, Route = "/hospitals/{id}")]
-        public int? Id { get; set; }
+        [ApiMember(ParameterType = "path", DataType = "integer", Format = "int32", IsRequired = false, Route = "/hospitals/{hospitalid}")]
+        public int? HospitalId { get; set; }
     }
 }
